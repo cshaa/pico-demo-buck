dc0facc7a4e792bf34ca219d41865c5b9bff9dd1
m93a <m93a.cz@gmail.com>
2024-09-25 15:35:25 +0200

    feat: buck convertor with a fixed duty cycle

--- main.c
+++ main.c
@@ -1,58 +1,51 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
+#include "hardware/pwm.h"
+#include "hardware/vreg.h"
 
-#ifdef PICO_W
-const uint LED_PIN = CYW43_WL_GPIO_LED_PIN;
-#else
-const uint LED_PIN = PICO_DEFAULT_LED_PIN;
-#endif
+const uint32_t CPU_FREQ = 250000;
+const enum vreg_voltage CPU_VOLTAGE = VREG_VOLTAGE_1_25;
+
+const uint32_t PWM_PIN = 0;
+const uint32_t PWM_WRAP = 255;
+const float duty_cycle = 0.4;
 
 uint32_t time_ms()
 {
     return (time_us_32() / 1000);
 }
 
-#ifdef PICO_W
-void led_setup()
-{
-    cyw43_arch_init();
-}
-void led_on()
-{
-    cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
-}
-void led_off()
-{
-    cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 0);
-}
-#else
-void led_setup()
-{
-    gpio_init(PICO_DEFAULT_LED_PIN);
-    gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
-}
-void led_on()
-{
-    gpio_put(PICO_DEFAULT_LED_PIN, true);
-}
-void led_off()
-{
-    gpio_put(PICO_DEFAULT_LED_PIN, false);
-}
-#endif
-
 int main()
 {
+    // Overclocking
+    vreg_set_voltage(CPU_VOLTAGE);
+    set_sys_clock_khz(CPU_FREQ, true);
+
+    // IO
     stdio_init_all();
 
-    led_setup();
+    // PWM
+    gpio_set_function(PWM_PIN, GPIO_FUNC_PWM);
+    uint slice = pwm_gpio_to_slice_num(PWM_PIN);
+    uint chan = pwm_gpio_to_channel(PWM_PIN);
+
+    uint32_t PWM_LEVEL = duty_cycle * (float)PWM_WRAP;
+    pwm_set_wrap(slice, PWM_WRAP);
+    pwm_set_chan_level(slice, chan, PWM_LEVEL);
+    pwm_set_output_polarity(slice, true, true);
+
+    pwm_set_enabled(slice, true);
+
+    // Onboard LED
+    gpio_init(PICO_DEFAULT_LED_PIN);
+    gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
 
     while (true)
     {
         printf("Hello, world! (%i)\n", time_ms() / 1000);
-        led_on();
+        gpio_put(PICO_DEFAULT_LED_PIN, true);
         sleep_ms(500);
-        led_off();
+        gpio_put(PICO_DEFAULT_LED_PIN, false);
         sleep_ms(500);
     }
 }
